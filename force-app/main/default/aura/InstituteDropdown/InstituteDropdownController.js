599e4670f54f6ac3b26ae82bdde84c903b1183e5
AasthaSahu7 <aastha.sahu@ksolves.com>
2024-03-12 12:24:58 +0530

    This is the VF page task in Aura

---
+++ force-app/main/default/aura/InstituteDropdown/InstituteDropdownController.js
@@ -0,0 +1,14 @@
+({
+    doInit: function(component, event, helper) {
+        helper.dropDown(component);
+    },
+    
+    getMentorTable: function(component, event, helper){
+        helper.mentorTable(component);
+    },
+    handleRowAction: function(component, event, helper){
+        var selectedRows = event.getParam('selectedRows');
+        helper.getStudentTable(component,selectedRows);
+    }
+    
+})
